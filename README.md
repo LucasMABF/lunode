d013ff6062b4f5c441f85cc5e0b496e18a5a7025
Lucas Mendes <70616909+LucasMABF@users.noreply.github.com>
2026-07-15 19:47:55 -0300

    chore: scaffold Cargo workspace with Nix dev env and CI

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lunode
+
+A Bitcoin full node written from scratch in pure Rust.
+
+This is an early work in progress, built primarily as a learning project.
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Lunode - A Bitcoin full node, built from the ground up in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.rustfmt = {
+            enable = true;
+            package = rustToolchain;
+          };
+          programs.nixfmt.enable = true;
+          programs.taplo.enable = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            rustToolchain
+          ];
+        };
+
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+      }
+    );
+}
